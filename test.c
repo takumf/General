1c4d06a7d5aed27ca85a4ef86703ae2b0fda250d
Takumf <takumfp@gmail.com>
2015-02-24 22:36:02 +0100

    Test

--- test.c
+++ test.c
@@ -4,6 +4,13 @@ int main (argc, argv)
 	int argc;
 	const char* argv[];
 {
-	printf ("This is a weird-ass convention.\n");
+	int i = 0;
+
+	if ( argc != 1)
+		for (i = 0; i < argc; ++i)
+			printf ("%s\n", argv [i]);
+	else
+		printf ("This is a weird-ass convention.\n");
+
 	return (0);
 }
